3a2385b7278b810c84e4751e2d7db6f30abdcf64
Alexandr <ElMaestroAlex@outlook.com>
2016-06-09 19:27:22 +0300

    init commit

---
+++ src/CubeIncPack/Main.java
@@ -0,0 +1,13 @@
+package CubeIncPack;
+
+/**
+ * Created by Александ on 09.06.2016.
+ */
+public class Main {
+
+    public static void main(String arg[]){
+        System.out.println(" CubeInc is starting a project!!!!!");
+
+    }
+
+}
